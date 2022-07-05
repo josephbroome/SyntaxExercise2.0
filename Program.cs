6a69c17b79ad6ebf704ad52dbf1bbbb46348b55b
Joseph Broome <josephlloydbroome@gmail.com>
2022-07-05 11:21:11 -0700

    syntax

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SyntaxExercise2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userGuess = int.Parse(Console.ReadLine());
+            var answer = 9;
+
+            var repsonse = (userGuess <answer) ? $"{userGuess} is less than mine" : $"{userGuess} is greater than or equal to mine";
+
+            Console.WriteLine(repsonse);
+
+        
+        
+        
+        }
+    }
+}
